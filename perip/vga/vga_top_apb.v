9ddc0bf896c658601caa9beeee073c5f052ed6ad
Gao Zhouhao <2996945797@qq.com>
2026-08-20 23:43:25 +0800

    sync local working tree

--- perip/vga/vga_top_apb.v
+++ perip/vga/vga_top_apb.v
@@ -20,4 +20,118 @@ module vga_top_apb(
   output        vga_valid
 );
 
+  wire [9:0] h_addr;
+  wire [9:0] v_addr;
+  wire [18:0] read_addr;
+  wire [18:0] write_addr;
+
+
+  wire [31:0] pixel_data;
+  assign read_addr = {9'b0, v_addr} * 640 + {9'b0, h_addr};
+  reg [31:0] fb [0:640*480-1];
+
+  assign write_addr = in_paddr[20:2];
+  assign pixel_data = fb[read_addr];
+
+  always @(posedge clock) begin
+    if (in_psel && in_penable && in_pready) begin
+      if (in_pwrite == 1'b1) begin
+        if (in_paddr >= 32'h2100_0000 && in_paddr <= (32'h2100_0000 + 640 * 480 * 4)) begin
+          fb[write_addr] <= in_pwdata;
+        end
+      end
+    end
+  end
+
+
+  vga_ctrl vga(
+    .pclk(clock),
+    .reset(reset),
+    .vga_data(pixel_data[23:0]),
+    .h_addr(h_addr),
+    .v_addr(v_addr),
+    .hsync(vga_hsync),
+    .vsync(vga_vsync),
+    .valid(vga_valid),
+    .vga_r(vga_r),
+    .vga_g(vga_g),
+    .vga_b(vga_b)
+  );
+
+  assign in_pready = 1'b1;
+  assign in_prdata  = 32'b0;
+  assign in_pslverr = 1'b0;
+
 endmodule
+
+
+
+
+module vga_ctrl(
+    input           pclk,     //25MHz时钟
+    input           reset,    //置位
+    input  [23:0]   vga_data, //上层模块提供的VGA颜色数据
+    output [9:0]    h_addr,   //提供给上层模块的当前扫描像素点坐标
+    output [9:0]    v_addr,
+    output          hsync,    //行同步和列同步信号
+    output          vsync,
+    output          valid,    //消隐信号
+    output [7:0]    vga_r,    //红绿蓝颜色信号
+    output [7:0]    vga_g,
+    output [7:0]    vga_b
+    );
+
+  //640x480分辨率下的VGA参数设置
+  parameter    h_frontporch = 96;
+  parameter    h_active = 144;
+  parameter    h_backporch = 784;
+  parameter    h_total = 800;
+
+  parameter    v_frontporch = 2;
+  parameter    v_active = 35;
+  parameter    v_backporch = 515;
+  parameter    v_total = 525;
+
+  //像素计数值
+  reg [9:0]    x_cnt;
+  reg [9:0]    y_cnt;
+  wire         h_valid;
+  wire         v_valid;
+
+  always @(posedge reset or posedge pclk) //行像素计数
+      if (reset == 1'b1)
+        x_cnt <= 1;
+      else
+      begin
+        if (x_cnt == h_total)
+            x_cnt <= 1;
+        else
+            x_cnt <= x_cnt + 10'd1;
+      end
+
+  always @(posedge pclk)  //列像素计数
+      if (reset == 1'b1)
+        y_cnt <= 1;
+      else
+      begin
+        if (y_cnt == v_total & x_cnt == h_total)
+            y_cnt <= 1;
+        else if (x_cnt == h_total)
+            y_cnt <= y_cnt + 10'd1;
+      end
+  //生成同步信号
+  assign hsync = (x_cnt > h_frontporch);
+  assign vsync = (y_cnt > v_frontporch);
+  //生成消隐信号
+  assign h_valid = (x_cnt > h_active) & (x_cnt <= h_backporch);
+  assign v_valid = (y_cnt > v_active) & (y_cnt <= v_backporch);
+  assign valid = h_valid & v_valid;
+  //计算当前有效像素坐标
+  assign h_addr = h_valid ? (x_cnt - 10'd145) : {10{1'b0}};
+  assign v_addr = v_valid ? (y_cnt - 10'd36) : {10{1'b0}};
+  //设置输出的颜色值
+  assign vga_r = vga_data[23:16];
+  assign vga_g = vga_data[15:8];
+  assign vga_b = vga_data[7:0];
+endmodule
+
