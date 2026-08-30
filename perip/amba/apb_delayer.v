5ff9af13f9dbedcfba0434beaea1d4a575962b8f
Gao Zhouhao <2996945797@qq.com>
2026-08-31 01:49:34 +0800

    refactor: update SoC simulation logic

--- perip/amba/apb_delayer.v
+++ perip/amba/apb_delayer.v
@@ -24,15 +24,72 @@ module apb_delayer(
   input         out_pslverr
 );
 
+  parameter ratio = 3;
+  parameter scale = 5;
+
+  typedef enum [1:0]{
+    idle_t,
+    wait_t,
+    delay_t
+  } state_t;
+  state_t state;
+  reg [31:0]  delay_cycle;
+  reg [31:0]  latency_count;
+
+  reg         pready;
+  reg [31:0]  prdata;
+  reg         pslverr;   
+  reg         delay_done;
+
+  always @(posedge clock) begin
+    if (reset == 1'b1) begin
+      delay_cycle <= 32'b0;
+      delay_done <= 1'b0;
+      latency_count <= 32'b0;
+      state <= idle_t;
+    end
+    else begin
+      case (state) 
+        idle_t: begin
+          if (in_penable && in_psel) begin
+            state <= wait_t;
+            delay_done <= 1'b0;
+            latency_count <= 32'b0;
+          end
+        end
+        wait_t: begin
+          delay_cycle <= delay_cycle + ratio * scale;
+          latency_count <= latency_count + 32'b1;
+          if (out_pready) begin
+            pready <= out_pready;
+            prdata <= out_prdata;
+            pslverr <= out_pslverr;
+            state <= delay_t;
+            delay_cycle <= delay_cycle / scale;
+          end
+        end
+        delay_t: begin
+          if (delay_cycle != latency_count) delay_cycle <= delay_cycle - 32'b1;
+          else begin
+            state <= idle_t;
+            delay_done <= 1'b1;
+          end
+        end
+        default:;
+      endcase
+    end
+  end
+
+
   assign out_paddr   = in_paddr;
-  assign out_psel    = in_psel;
-  assign out_penable = in_penable;
+  assign out_psel    = (state == wait_t) ? in_psel : 1'b0; 
+  assign out_penable = (state == wait_t) ? in_penable : 1'b0;
   assign out_pprot   = in_pprot;
   assign out_pwrite  = in_pwrite;
   assign out_pwdata  = in_pwdata;
   assign out_pstrb   = in_pstrb;
-  assign in_pready   = out_pready;
-  assign in_prdata   = out_prdata;
-  assign in_pslverr  = out_pslverr;
+  assign in_pready   = delay_done ? pready : 1'b0;
+  assign in_prdata   = delay_done ? prdata : 32'b0;
+  assign in_pslverr  = delay_done ? pslverr : 1'b0;
 
 endmodule
