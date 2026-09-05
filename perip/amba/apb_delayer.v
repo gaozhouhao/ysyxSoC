539f90620017ec0f1e8ea3ea958c581b00569e2b
Gao Zhouhao <2996945797@qq.com>
2026-09-06 03:24:51 +0800

    fix: correct APB delayer transaction handling

--- perip/amba/apb_delayer.v
+++ perip/amba/apb_delayer.v
@@ -24,102 +24,76 @@ module apb_delayer(
   input         out_pslverr
 );
 
-    parameter ratio = 3;
-    parameter scale = 5;
+  parameter ratio = 3;
+  parameter scale = 5;
 
-    typedef enum [1:0]{
-        idle_t,
-        wait_t,
-        delay_t,
-        resp_t
-    } state_t;
-    state_t state;
+  typedef enum [1:0]{
+    idle_t,
+    wait_t,
+    delay_t
+  } state_t;
+  state_t state;
+  reg [31:0]  delay_cycle;
+  reg [31:0]  latency_count;
 
-    reg [31:0] delay_cycle;
-    reg [31:0] latency_count;
+  reg         pready;
+  reg [31:0]  prdata;
+  reg         pslverr;   
+  reg         delay_done;
 
-    reg [31:0] prdata;
-    reg        pslverr;
-
-    always @(posedge clock) begin
-        if (reset == 1'b1) begin
-            state <= idle_t;
-            delay_cycle <= 32'b0;
-            latency_count <= 32'b0;
-            prdata <= 32'b0;
-            pslverr <= 1'b0;
-        end
-        else begin
-            case (state)
-                idle_t: begin
-                if (in_psel && in_penable) begin
-                    state <= wait_t;
-                    delay_cycle <= 32'b0;
-                    latency_count <= 32'b0;
-                end
-                end
-                wait_t: begin
-                    if (in_psel && in_penable) begin
-                        latency_count <= latency_count + 32'b1;
-
-                        if (out_pready) begin
-                            prdata <= out_prdata;
-                            pslverr <= out_pslverr;
-
-                            if (((delay_cycle + ratio * scale) / scale) > (latency_count + 32'b1)) begin
-                                delay_cycle <= (delay_cycle + ratio * scale) / scale - latency_count - 32'b1;
-                                state <= delay_t;
-                            end
-                            else begin
-                                delay_cycle <= 32'b0;
-                                state <= resp_t;
-                            end
-                        end
-                        else begin
-                            delay_cycle <= delay_cycle + ratio * scale;
-                        end
-                    end
-                    else begin
-                        state <= idle_t;
-                        delay_cycle <= 32'b0;
-                        latency_count <= 32'b0;
-                    end
-                end
-
-                delay_t: begin
-                    if (delay_cycle > 32'b1) begin
-                        delay_cycle <= delay_cycle - 32'b1;
-                    end
-                    else begin
-                        delay_cycle <= 32'b0;
-                        state <= resp_t;
-                    end
-                end
-
-                resp_t: begin
-                    if (in_psel && in_penable) begin
-                        state <= idle_t;
-                    end
-                end
-
-                default: begin
-                    state <= idle_t;
-                end
-            endcase
-        end
+  always @(posedge clock) begin
+    if (reset == 1'b1) begin
+      delay_cycle <= 32'b0;
+      delay_done <= 1'b0;
+      latency_count <= 32'b0;
+      state <= idle_t;
     end
+    else begin
+      case (state) 
+        idle_t: begin
+            if (delay_done) delay_done <= 1'b0;
+            if (in_penable && in_psel) begin
+                state <= wait_t;
+                delay_done <= 1'b0;
+                latency_count <= 32'b0;
+                delay_cycle <= 32'b0;
+            end
+        end
+        wait_t: begin
+          latency_count <= latency_count + 32'b1;
+          if (out_pready) begin
+            pready <= out_pready;
+            prdata <= out_prdata;
+            pslverr <= out_pslverr;
+            state <= delay_t;
+            delay_cycle <= delay_cycle / scale;
+          end
+          else begin
+            delay_cycle <= delay_cycle + ratio * scale;
+          end
+        end
+        delay_t: begin
+          if (delay_cycle > latency_count) delay_cycle <= delay_cycle - 32'b1;
+          else begin
+            state <= idle_t;
+            delay_done <= 1'b1;
+          end
+        end
+        default:;
+      endcase
+    end
+  end
 
-    assign out_paddr   = in_paddr;
-    assign out_psel    = (state == wait_t) ? in_psel : 1'b0;
-    assign out_penable = (state == wait_t) ? in_penable : 1'b0;
-    assign out_pprot   = in_pprot;
-    assign out_pwrite  = in_pwrite;
-    assign out_pwdata  = in_pwdata;
-    assign out_pstrb   = in_pstrb;
 
-    assign in_pready   = (state == resp_t) ? 1'b1 : 1'b0;
-    assign in_prdata   = (state == resp_t) ? prdata : 32'b0;
-    assign in_pslverr  = (state == resp_t) ? pslverr : 1'b0;
+  assign out_paddr   = in_paddr;
+  assign out_psel    = (state == wait_t) ? in_psel : 1'b0; 
+  assign out_penable = (state == wait_t) ? in_penable : 1'b0;
+  assign out_pprot   = in_pprot;
+  assign out_pwrite  = in_pwrite;
+  assign out_pwdata  = in_pwdata;
+  assign out_pstrb   = in_pstrb;
+  assign in_pready   = delay_done ? pready : 1'b0;
+  assign in_prdata   = delay_done ? prdata : 32'b0;
+  assign in_pslverr  = delay_done ? pslverr : 1'b0;
 
 endmodule
-
