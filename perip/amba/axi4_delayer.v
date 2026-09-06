080aa6fe26606575828d0fcffa0279ef8631f6d5
Gao Zhouhao <2996945797@qq.com>
2026-09-06 22:07:46 +0800

    perf: model AXI memory latency in simulation

--- perip/amba/axi4_delayer.v
+++ perip/amba/axi4_delayer.v
@@ -63,34 +63,143 @@ module axi4_delayer(
   input  [1:0]  out_bresp
 );
 
-  assign in_arready = out_arready;
-  assign out_arvalid = in_arvalid;
-  assign out_arid = in_arid;
-  assign out_araddr = in_araddr;
-  assign out_arlen = in_arlen;
-  assign out_arsize = in_arsize;
-  assign out_arburst = in_arburst;
-  assign out_rready = in_rready;
-  assign in_rvalid = out_rvalid;
-  assign in_rid = out_rid;
-  assign in_rdata = out_rdata;
-  assign in_rresp = out_rresp;
-  assign in_rlast = out_rlast;
-  assign in_awready = out_awready;
-  assign out_awvalid = in_awvalid;
-  assign out_awid = in_awid;
-  assign out_awaddr = in_awaddr;
-  assign out_awlen = in_awlen;
-  assign out_awsize = in_awsize;
-  assign out_awburst = in_awburst;
-  assign in_wready = out_wready;
-  assign out_wvalid = in_wvalid;
-  assign out_wdata = in_wdata;
-  assign out_wstrb = in_wstrb;
-  assign out_wlast = in_wlast;
-  assign out_bready = in_bready;
-  assign in_bvalid = out_bvalid;
-  assign in_bid = out_bid;
-  assign in_bresp = out_bresp;
+    parameter ratio = 3; 
+    parameter scale = 5; 
+
+
+    typedef enum [1:0] {
+        read_idle,
+        wait_arready,
+        delay_arready,
+        read_data
+    } read_state_t;
+
+    read_state_t read_state;
+
+    reg [31:0] response_schedule [0:256];
+
+    reg [31:0] latency_count;
+
+    reg [7:0] num_of_data;
+    reg [7:0] num_have_send;
+
+
+    typedef struct packed {
+        reg [3:0]  id;
+        reg [31:0] data;
+        reg [1:0]  resp;
+        reg        last;
+    } rdata_t;
+
+    rdata_t data_array [0:255];
+
+    assign out_arvalid = ((read_state == read_idle) || (read_state == wait_arready)) ? in_arvalid : 1'b0;
+
+    assign out_arid    = in_arid;
+    assign out_araddr  = in_araddr;
+    assign out_arlen   = in_arlen;
+    assign out_arsize  = in_arsize;
+    assign out_arburst = in_arburst;
+
+
+    assign in_arready = (read_state == read_idle)       ? out_arready :
+                        (read_state == delay_arready)   ? ((latency_count + 1) >= response_schedule[0]) : 
+                        1'b0;
+
+
+    assign out_rready = (read_state != read_idle);
+
+
+    assign in_rvalid =
+        (read_state == read_data) &&
+        (num_have_send < num_of_data) &&
+        ((latency_count + 1) >=
+         response_schedule[num_have_send + 1]);
+
+
+    assign in_rid   = data_array[num_have_send].id;
+    assign in_rdata = data_array[num_have_send].data;
+    assign in_rresp = data_array[num_have_send].resp;
+    assign in_rlast = data_array[num_have_send].last;
+
+    always @(posedge clock) begin
+        if (reset) begin
+            read_state    <= read_idle;
+            latency_count <= 32'b0;
+            num_of_data      <= 8'b0;
+            num_have_send    <= 8'b0;
+        end
+        else begin
+            case (read_state)
+                read_idle: begin
+                    if (in_arvalid) begin
+                        latency_count <= 32'b0;
+                        num_of_data   <= 8'b0;
+                        num_have_send <= 8'b0;
+                        if (out_arready) begin
+                            read_state <= read_data;
+                        end
+                        else begin
+                            read_state <= wait_arready;
+                        end
+                    end
+                end
+                wait_arready: begin
+                    latency_count <= latency_count + 1'b1;
+                    if (out_arready) begin
+                        response_schedule[0] <= (latency_count + 1) * ratio * scale;
+                        read_state <= delay_arready;
+                    end
+                end
+                delay_arready: begin
+                    latency_count <= latency_count + 1'b1;
+                    if (in_arvalid && in_arready) begin
+                        read_state <= read_data;
+                    end
+                end
+                read_data: begin
+                    latency_count <= latency_count + 1'b1;
+                    if (in_rvalid && in_rready) begin
+                        num_have_send <= num_have_send + 1'b1;
+                        if (in_rlast) begin
+                            read_state <= read_idle;
+                        end
+                    end
+                end
+            endcase
+
+            if ((read_state != read_idle) && out_rvalid && out_rready) begin
+                data_array[num_of_data].id   <= out_rid;
+                data_array[num_of_data].data <= out_rdata;
+                data_array[num_of_data].resp <= out_rresp;
+                data_array[num_of_data].last <= out_rlast;
+                response_schedule[num_of_data + 1] <= (latency_count + 1) * ratio * scale;
+                num_of_data <= num_of_data + 1'b1;
+            end
+
+        end
+    end
+
+
+
+    assign in_awready = out_awready;
+    assign out_awvalid = in_awvalid;
+    assign out_awid = in_awid;
+    assign out_awaddr = in_awaddr;
+    assign out_awlen = in_awlen;
+    assign out_awsize = in_awsize;
+    assign out_awburst = in_awburst;
+
+    assign in_wready = out_wready;
+    assign out_wvalid = in_wvalid;
+    assign out_wdata = in_wdata;
+    assign out_wstrb = in_wstrb;
+    assign out_wlast = in_wlast;
+
+    assign out_bready = in_bready;
+    assign in_bvalid = out_bvalid;
+    assign in_bid = out_bid;
+    assign in_bresp = out_bresp;
+
 
 endmodule
